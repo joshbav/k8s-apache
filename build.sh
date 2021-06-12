fe6c18a8794fe4520c76bbd2f6afd510788fe7cd
“joshbav” <josh@laptop.home>
2021-06-12 15:29:17 -0700

    Scripted commit 12-Jun-2021-03:29:17PM

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo Creating 1.yaml
 ./1.sh
-# This is just to update git
+
+echo
+echo
+echo Adding all files in repo and pushing to git
 git config user.name “joshbav” 
 git add -A
 git commit -m "Scripted commit $(date +%d-%b-%Y-%I:%M:%S%p)"
