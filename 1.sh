0a65b2c194f148f88e9c0f91fdd05d19106d2ae9
“joshbav” <josh@laptop.home>
2021-06-12 14:41:41 -0700

    Scripted commit 12-Jun-2021-02:41:41PM

--- 1.sh
+++ 1.sh
@@ -1,15 +1,19 @@
-    echo
-    echo Creating one big yaml file, 1.yaml, with all existing .yamls
-    echo so one kubectl apply can create them all
-    echo
-    echo First deleting existing 1.yaml by moving it to /tmp
-    mv 1.yaml /tmp >/dev/null 2>&1
-    echo
+#!/bin/bash
+
+# CREATES ONE BIG YAML FILE, 1.YAML, FOR EASY KUBECTRL APPLY. 
+
+echo
+echo Creating one big yaml file, 1.yaml, with all existing .yamls
+echo so one kubectl apply can create them all
+echo
+echo First deleting existing 1.yaml by moving it to /tmp
+mv 1.yaml /tmp >/dev/null 2>&1
+echo
 
 # Going to use arrays, so enable nullglob in case there are no matching files
 shopt -s nullglob
 
-### Do namespaces first
+### CREATE NAMESPACES AT TOP OF FILE
 
 ns=(ns_*.yaml)
 
@@ -20,11 +24,10 @@ for ((i=0; i<${#ns[@]}; i++)); do
     echo >>1.yaml
     echo "#################################################" >>1.yaml
     echo "################# NEW YAML OBJECT ###############" >>1.yaml
-    echo "#################################################" >>1.yaml
     echo "---">>1.yaml
 done
 
-### Services  must be second, since you must create them before the deployment
+### CREATE SERVCIES NEXT, SINCE THEY SHOULD EXIT BEFORE PODS/DEPLOYMENTS/ETC
 
 svc=(svc_*.yaml)
 
@@ -35,11 +38,10 @@ for ((i=0; i<${#svc[@]}; i++)); do
     echo >>1.yaml
     echo "#################################################" >>1.yaml
     echo "################# NEW YAML OBJECT ###############" >>1.yaml
-    echo "#################################################" >>1.yaml
     echo "---">>1.yaml
 done
 
-### Now everything else
+### NOW ADD EVERYTHING ELSE SINCE THE ORDERING OF THE REST DOESN'T MATTER
 
 files=(*.yaml)
 
@@ -62,6 +64,5 @@ for ((i=0; i<${#files[@]}; i++)); do
     echo >>1.yaml
     echo "#################################################" >>1.yaml
     echo "################# NEW YAML OBJECT ###############" >>1.yaml
-    echo "#################################################" >>1.yaml
     echo "---">>1.yaml
 done
